e9659f928773910d55a9b4f260396276562d0628
Yuan Wang <wang17@ualberta.ca>
2021-01-25 23:48:29 +0800

    add cors

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors')
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors())
 
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
