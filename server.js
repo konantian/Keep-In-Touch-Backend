752bc3518dbfd4ed8e23148971ce4de77803929f
Yuan Wang <wang17@ualberta.ca>
2021-02-03 09:03:04 -0700

    modify message

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ io.on('connection',socket => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
